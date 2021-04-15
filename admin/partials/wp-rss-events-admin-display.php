ffbcd8aafcab3d67aeac2c8ecceb821113da2429
Nauman <naumanahmed19@gmail.com>
2021-04-15 15:21:41 +0200

    enh: import events admin template

--- admin/partials/wp-rss-events-admin-display.php
+++ admin/partials/wp-rss-events-admin-display.php
@@ -11,5 +11,23 @@
  * @package    Wp_Rss_Events
  * @subpackage Wp_Rss_Events/admin/partials
  */
+
 ?>
-Hellow WOlrd
+<div id="welcome-panel" class="welcome-panel">	
+	<div class="welcome-panel-content">
+		<h2>Import Data</h2>
+		
+		<div class="welcome-panel-column-container">
+		<div class="welcome-panel-column">
+				<div class="rss-event-loader">
+					<img  src="<?php echo  get_site_url(); ?>/wp-admin/images/spinner.gif" />
+				</div>
+				<a class="button button-primary button-hero load-customize btn-rss-event-import" href="#">
+					Import Events
+				</a>
+				<p class="hide-if-no-customize">	</p>
+			</div>
+		</div>
+	</div>
+</div>
+
