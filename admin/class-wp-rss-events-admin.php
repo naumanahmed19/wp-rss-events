c8a7082da89d801363e4674cdd08baf9903227c4
Nauman <naumanahmed19@gmail.com>
2021-04-15 18:43:02 +0200

    enh: import post thumbnail

--- admin/class-wp-rss-events-admin.php
+++ admin/class-wp-rss-events-admin.php
@@ -146,7 +146,7 @@ class Wp_Rss_Events_Admin {
 			'label'                 => __( 'Event', 'text_domain' ),
 			'description'           => __( 'Event Description', 'text_domain' ),
 			'labels'                => $labels,
-			'supports'              => array( 'title', 'editor' ),
+			'supports'              => array( 'title', 'editor','thumbnail' ),
 			'taxonomies'            => array( 'category', 'post_tag' ),
 			'hierarchical'          => false,
 			'public'                => true,
@@ -228,18 +228,82 @@ class Wp_Rss_Events_Admin {
 			   $custom_label_2 =  $this->toDate($feed[$x]['custom_label_2']);
 			   $custom_label_1 =   $this->toDate($feed[$x]['custom_label_1']);
 			   $custom_label_3 = $feed[$x]['custom_label_3'];
+
+			  
 	   
 			   $post_information = array(
 				   'post_title' =>$title,
 				   'post_content' => $description,
 				   'post_type' => 'events',
 				   'post_status' => 'publish',
-				   'post_date' => date('Y-m-d H:i:s')
+				   'post_date' => date('Y-m-d H:i:s'),
+					'meta_input' => array(
+						'link' =>  $link,
+						'custom_label_0'=>  $custom_label_0 ,
+						'custom_label_1' => $custom_label_1 ,
+						'custom_label_2' => $custom_label_2 ,
+						'custom_label_3'=> $custom_label_3 ,
+						'price'=> $price ,
+						'availability'=>  $availability ,
+						'availability_date'=>  $availability_date ,
+						'expiration_date' =>  $expiration_date ,
+					),
 			   );
+
+			  
+			 
 	   
-			   wp_insert_post( $post_information );    
+			   $post_id  =  wp_insert_post( $post_information );    
+
+			   $this->attach_thumbnail($post_id , $image_link );	
 		
 		   }
+
+	}
+
+	function attach_thumbnail($post_id,$image_url){
+
+		$image_name       =basename($image_url);
+		$upload_dir       = wp_upload_dir(); // Set upload folder
+		$image_data       = file_get_contents($image_url); // Get image data
+		$unique_file_name = wp_unique_filename( $upload_dir['path'], $image_name ); // Generate unique name
+		$filename         = basename( $unique_file_name ); // Create image file name
+		
+		// Check folder permission and define file location
+		if( wp_mkdir_p( $upload_dir['path'] ) ) {
+			$file = $upload_dir['path'] . '/' . $filename;
+		} else {
+			$file = $upload_dir['basedir'] . '/' . $filename;
+		}
+		
+		// Create the image  file on the server
+		file_put_contents( $file, $image_data );
+		
+		// Check image file type
+		$wp_filetype = wp_check_filetype( $filename, null );
+		
+		// Set attachment data
+		$attachment = array(
+			'post_mime_type' => $wp_filetype['type'],
+			'post_title'     => sanitize_file_name( $filename ),
+			'post_content'   => '',
+			'post_status'    => 'inherit'
+		);
+		
+		// Create the attachment
+		$attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+		
+		// Include image.php
+		require_once(ABSPATH . 'wp-admin/includes/image.php');
+		
+		// Define attachment metadata
+		$attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+		
+		// Assign metadata to attachment
+		wp_update_attachment_metadata( $attach_id, $attach_data );
+		
+		// And finally assign featured image to post
+		set_post_thumbnail( $post_id, $attach_id );
 
 	}
 
